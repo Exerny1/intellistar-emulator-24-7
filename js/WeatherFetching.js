4f783d840dc0a32ba11137ffb3d1edfb50eae7a3
Exerny1 <rodrigo.coolkid@gmail.com>
2026-03-01 11:46:42 -0500

    Update WeatherFetching.js

--- js/WeatherFetching.js
+++ js/WeatherFetching.js
@@ -1,3 +1,5 @@
+var timelineStarted = false;
+
 function guessZipCode(){
   return;
 }
@@ -13,13 +15,13 @@ function fetchAlerts(){
         }
         for(var i = 0; i < data.features.length; i++){
           alertCrawl = alertCrawl + " " + data.features[i].properties.description.replace("...", " ");
-          alerts[i] = data.features[i].properties.event
+          alerts[i] = data.features[i].properties.event;
         }
         if(alertCrawl != ""){ CONFIG.crawl = alertCrawl; }
         alertsActive = alerts.length > 0;
         fetchForecast();
       });
-    })
+    });
 }
 
 function fetchForecast(){
@@ -34,13 +36,13 @@ function fetchForecast(){
           forecastTemp[i] = n.temp;
           forecastIcon[i] = n.icon_code;
           forecastNarrative[i] = n.narrative;
-          forecastPrecip[i] = `${n.pop}% Chance<br/> of ${n.precip_type}`;
+          forecastPrecip[i] = `${n.pop}% Chance<br/> of ${n.precip_type.charAt(0).toUpperCase() + n.precip_type.substr(1).toLowerCase()}`;
         }
         for (var i = 0; i < 7; i++) {
           let fc = forecasts[i+1];
           outlookHigh[i] = fc.max_temp;
           outlookLow[i] = fc.min_temp;
-          outlookCondition[i] = (fc.day ? fc.day : fc.night).phrase_32char;
+          outlookCondition[i] = (fc.day ? fc.day : fc.night).phrase_32char.split(' ').join('<br/>').replace("Thunderstorm", "Thunder</br>storm");
           outlookIcon[i] = (fc.day ? fc.day : fc.night).icon_code;
         }
         fetchRadarImages();
@@ -56,7 +58,7 @@ function fetchCurrentWeather(){
     if(airportCodeLength==3){location=`iataCode=${airportCode}`}
     else if (airportCodeLength==4){location=`icaoCode=${airportCode}`}
   }
-
+  
   fetch(`https://api.weather.com/v3/location/point?${location}&language=${CONFIG.language}&format=json&apiKey=${CONFIG.secrets.twcAPIKey}`)
       .then(function (response) {
       response.json().then(function(data) {
@@ -69,11 +71,18 @@ function fetchCurrentWeather(){
               let unit = data.observation[CONFIG.unitField];
               currentTemperature = Math.round(unit.temp);
               currentCondition = data.observation.phrase_32char;
-              windSpeed = `${data.observation.wdir_cardinal} ${unit.wspd}`;
+              windSpeed = `${data.observation.wdir_cardinal} ${unit.wspd} ${CONFIG.units === 'm' ? 'km/h' : 'mph'}`;
               gusts = unit.gust || 'NONE';
               feelsLike = unit.feels_like;
-              humidity = unit.rh;
+              
+              // FIX: Ensure these are clean numbers for the "fast counting" aesthetics
+              visibility = Number(unit.vis);
+              humidity = Number(unit.rh);
+              dewPoint = Number(unit.dewpt);
+              
               pressure = unit.altimeter.toPrecision(4);
+              let ptendCode = data.observation.ptend_code;
+              pressureTrend = (ptendCode == 1 || ptendCode == 3) ? '▲' : ptendCode == 0 ? '' : '▼'; 
               currentIcon = data.observation.icon_code;
               fetchAlerts();
             });
@@ -83,40 +92,33 @@ function fetchCurrentWeather(){
 }
 
 function fetchRadarImages(){
-  var radarContainer = document.getElementById('radar-container');
-  if (!radarContainer) return;
+  var radarContainer = getElement('radar-container');
+  if(radarContainer) radarContainer.innerHTML = "";
   
-  radarContainer.innerHTML = ""; 
   var radarImage = document.createElement("iframe");
-  
   var mapSettings = btoa(JSON.stringify({
     "agenda": { "id": "weather", "center": [longitude, latitude], "zoom": 8 },
     "animating": true, "base": "standard", "opacity": { "national": 0.6 }
   }));
 
-  radarImage.src = "https://radar.weather.gov/?settings=v1_" + mapSettings;
+  radarImage.setAttribute("src", "https://radar.weather.gov/?settings=v1_" + mapSettings);
   
-  // FIXED RADAR CROPPING
+  // Radar Cropping
   radarImage.style.width = "1600px";
-  radarImage.style.height = "900px";
-  radarImage.style.marginTop = "-280px"; // Adjust this to crop the top header off
-  radarImage.style.marginLeft = "-185px"; // Adjust this to center the map
+  radarImage.style.height = "1000px";
+  radarImage.style.marginTop = "-280px"; 
+  radarImage.style.marginLeft = "-180px";
   radarImage.style.border = "none";
-  radarImage.style.position = "relative";
   
-  radarContainer.appendChild(radarImage);
-  radarContainer.style.overflow = "hidden"; // Clips the edges of the iframe
+  if(radarContainer) radarContainer.appendChild(radarImage);
 
-  // THE OVERLAP FIX: Only start the timeline if it hasn't started yet
-  if (typeof timelineStarted === 'undefined') {
-      window.timelineStarted = true;
-      if (typeof scheduleTimeline === "function") {
-          scheduleTimeline();
-      }
+  if (!timelineStarted) {
+    scheduleTimeline();
+    timelineStarted = true;
   }
 }
 
-// Update variables every 60 seconds without restarting the slides
+// 1-Minute Silent Refresh
 setInterval(function() {
   fetchCurrentWeather();
 }, 60000);
